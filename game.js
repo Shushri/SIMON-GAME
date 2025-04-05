1227f460f4ac0174ef9ac7473390b00efa34c8f7
Shushri <shubhiarnm@gmail.com>
2025-04-05 19:20:36 +0530

    added condition

--- game.js
+++ game.js
@@ -3,11 +3,14 @@ var gamePattern=[];
 var userClickedPattern=[];
 var level=0;
 function nextsequence(){
+  
   $("h1").text("Level "+level);
   level=level+1;
+  
   randomNumber=Math.floor(Math.random()*4);
   randomChosenColour=buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+  //button animation and sound
   $("#"+randomChosenColour).fadeOut().fadeIn();
   var audio=new Audio("sounds/"+randomChosenColour+".mp3");
   audio.play(); 
@@ -15,16 +18,29 @@ function nextsequence(){
 
 $(".btn").on("click",function(){
   var userChosenColour=$(this).attr("id");
+  //button animation and sound
   $("#"+userChosenColour).fadeOut().fadeIn();
-  userClickedPattern.push(userChosenColour);
-  check_answer();
   var audio=new Audio("sounds/"+userChosenColour+".mp3");
   audio.play();
 
+  userClickedPattern.push(userChosenColour);
+  var len=userClickedPattern.length;
+  check_answer(len);
+  
+
 })
 
-function check_answer(currentlevel){
-
+function check_answer(len){
+    
+    if(gamePattern[len-1]!=userClickedPattern[len-1]){
+      $("h1").text("GAME OVER!!!");
+      var audio=new Audio("sounds/wrong.mp3");
+      audio.play();
+    }
+    else if(len===gamePattern.length){
+      nextsequence();
+    }
+    
 }
 
 $(document).on("keypress",nextsequence)
